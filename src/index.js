c92a281ab11acd609af75d2d83ec061028b5df9b
Ann <ann.arzamm@gmail.com>
2021-04-27 22:23:20 +0700

    + animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './index.scss';
+
+// sandwich menu
+const menu = document.querySelector('.sandwich');
+const dropdown = document.querySelector('.sandwich__dropdown');
+
+menu.addEventListener('click', (e) => {
+  e.preventDefault();
+  dropdown.classList.toggle('sandwich__dropdown_active');
+});
+
+// page up button
+const upElem = document.querySelector('.pageup');
+
+// window.addEventListener('scroll', () => {
+//   if (document.documentElement.scrollTop > 100) {
+//     upElem.classList.add('pageup_active');
+//   }
+// });
