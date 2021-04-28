4a8afeedcf2700d54eadd6743e597e6c268ffd94
Ann <ann.arzamm@gmail.com>
2021-04-28 17:23:42 +0700

    header & links on all pages

--- src/index.js
+++ src/index.js
@@ -15,11 +15,9 @@ const upElem = document.querySelector('.pageup');
 
 window.addEventListener('scroll', () => {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    // upElem.style.bottom = '40px';
     upElem.classList.add('animate__fadeInUp');
     upElem.classList.remove('animate__fadeOutDown');
   } else {
-    // upElem.style.bottom = '-100%';
     upElem.classList.add('animate__fadeOutDown');
     upElem.classList.remove('animate__fadeInUp');
   }
